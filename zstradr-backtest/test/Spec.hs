bbea328399cdeffd9da88b38a7d6a6d0af639ed9
Forge <forge@zstradr.dev>
2026-02-19 21:40:51 -0800

    chore: initial empty commit

--- zstradr-backtest/test/Spec.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import Test.Hspec
-
-main :: IO ()
-main = hspec $ do
-  describe "ZStradr.Backtest.Engine" $ do
-    it "placeholder - engine tests to be added" $
-      True `shouldBe` True
